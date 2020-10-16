7b3f93635239c39dccdd53d23967bdf289151fcc
nicolesayshi <nlnguyen1231@gmail.com>
2020-10-15 22:09:18 -0700

    importing textures to Nicolsayshi file
    don't mind this, i'm just adding some stuff for me to test out.

---
+++ Assets/NicoleSaysHi/TextHealthUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TextHealthUI : MonoBehaviour
+{
+    public Player current_player;
+    public Text Health_text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Health_text.text = "Health : " + current_player.health + " / " + current_player.maxHealth;
+    }
+}
