1befd2088a63d4ea1df59e4335e92c8b91373cdb
WaterKat_ROG <WaterKatGames@gmail.com>
2020-10-17 22:00:08 -0700

    Created Snow effects in tutorial scene

---
+++ Assets/WaterKat/Player/Debug_Wind.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debug_Wind : MonoBehaviour
+{
+    private new Rigidbody rigidbody;
+    public Vector3 windVelocity = new Vector3(-10, 0, 0);
+    public float velocityThreshold = 2f;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (rigidbody.velocity.magnitude>velocityThreshold)
+            rigidbody.AddForce(windVelocity);
+    }
+}
